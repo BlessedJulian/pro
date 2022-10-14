9222b75fed486384f01bdf5765624eb9b3959482
BlessedJulian <ogbajijulian@gmail.com>
2022-10-14 14:09:33 +0100

    index commit

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from "express"
-
+import env from "dotenv"
 
 const app = express()
 
+env.config()
 
-app.listen()
+
+
+
+
+app.listen(process.env.PORT, (error) => {
+
+    if(error) console.log({message: 'connection failed'})
+
+    console.log(`app started, running on port ${process.env.PORT}......`)
+})
